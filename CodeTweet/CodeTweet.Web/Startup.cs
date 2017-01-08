fbff7e639ed23a9f206dfb2d32ee6a27fd769d4f
Amir Zuker <amirz@codevalue.net>
2017-01-08 22:48:25 +0200

    oooo

--- CodeTweet/CodeTweet.Web/Startup.cs
+++ CodeTweet/CodeTweet.Web/Startup.cs
@@ -7,8 +7,8 @@ namespace CodeTweet.Web
         public void Configuration(IAppBuilder app)
         {
             //something new!
-            //something new!
             ConfigureAuth(app);
+            //aaaa
         }
     }
 }
